0b8aca1604a94cecf8b3c222f7599140e0617144
Tyler Parsons <tyjparsons@ou.edu>
2021-03-07 21:40:20 -0600

    like 2 hours of work with no progress

--- JSONRickAndMorty/MainWindow.xaml.cs
+++ JSONRickAndMorty/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +25,51 @@ namespace JSONRickAndMorty
         public MainWindow()
         {
             InitializeComponent();
+
+           string url = "https://rickandmortyapi.com/api/character";
+
+            //using (var client = new HttpClient())
+            //{
+            //    string jsonData = client.GetStringAsync(url).Result;
+
+            //    RickAndMortyAPI api = JsonConvert.DeserializeObject<RickAndMortyAPI>(jsonData);
+
+            //    foreach (var character in api.results)
+            //    {
+            //        lstCharacters.Items.Add(character);
+            //    }
+            //}
+            using (var client = new HttpClient())
+            {
+                string jsonData = client.GetStringAsync(url).Result;
+
+                RickAndMortyAPI api = JsonConvert.DeserializeObject<RickAndMortyAPI>(jsonData);
+
+                //foreach (var character in api.results)
+                //{
+                //    lstCharacters.Items.Add(character);
+                //}
+
+                string urlNext = api.info.next;
+
+                string jsonData2 = client.GetStringAsync(urlNext).Result;
+
+                RickAndMortyAPI api2 = JsonConvert.DeserializeObject<RickAndMortyAPI>(jsonData2);
+
+                foreach (var character in api.results)
+                {
+                    lstCharacters.Items.Add(character);
+
+                }
+                
+
+
+            }
+        }
+        private void lstCharacters_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedCharacter = (Character)lstCharacters.SelectedItem;
+            imgCharacter.Source = new BitmapImage(new Uri(selectedCharacter.image));
         }
     }
 }
